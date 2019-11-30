ba1f19a1a84f5d498e6efd94305e0e5a91e4566a
Rehan Ashraf <rashraf.cheema@gmail.com>
2019-11-29 22:09:53 -0800

    Seprated projects for Eclipse and Xcode
    Signed-off-by: Rehan Ashraf <rashraf.cheema@gmail.com>

---
+++ leetcodeXcode/source/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcodeXcode
+//
+//  Created by Rehan Ashraf on 11/29/19.
+//  Copyright © 2019 Rehan Ashraf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
